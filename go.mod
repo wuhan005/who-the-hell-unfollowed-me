c86bd26b2b9e5ed4fc304251c9e40c04d254bd50
E99p1ant <i@github.red>
2023-02-11 02:57:14 +0800

    initial implementation

--- go.mod
+++ go.mod
@@ -1,7 +1,23 @@
-module github.com/wuhan005/go-template
+module github.com/wuhan005/who-the-hell-unfollowed-me
 
 go 1.19
 
-require github.com/samber/lo v1.29.0
+require (
+	github.com/google/go-github/v50 v50.0.0
+	github.com/samber/lo v1.37.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	unknwon.dev/clog/v2 v2.2.0
+)
 
-require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+require (
+	github.com/fatih/color v1.7.0 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.4 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,104 @@
 package main
 
 import (
+	"context"
 	"os"
-	"os/signal"
-	"syscall"
+	"strings"
 
-	_ "github.com/samber/lo"
+	"github.com/google/go-github/v50/github"
+	"github.com/samber/lo"
+	"golang.org/x/oauth2"
+	log "unknwon.dev/clog/v2"
 )
 
+const unfollowedEventFileName = "unfollowed.txt"
+
 func main() {
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-	<-sig
+	defer log.Stop()
+	if err := log.NewConsole(); err != nil {
+		panic("init log: " + err.Error())
+	}
+
+	githubToken := os.Getenv("GH_TOKEN")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// Get current user.
+	currentUser, _, err := client.Users.Get(ctx, "")
+	if err != nil {
+		log.Fatal("Failed to get current user: %v", err)
+	}
+	log.Info("Current user: %s", currentUser.GetLogin())
+
+	// Get user's follower list.
+	var currentFollowerIDs []string
+	currentPage := 1
+	for {
+		log.Trace("Fetching followers, page %d", currentPage)
+
+		followers, _, err := client.Users.ListFollowers(ctx, currentUser.GetLogin(), &github.ListOptions{
+			Page:    currentPage,
+			PerPage: 100,
+		})
+		if err != nil {
+			log.Fatal("Failed to get user's followers, page: %d: %v", currentPage, err)
+		}
+
+		if len(followers) == 0 {
+			break
+		}
+
+		for _, follower := range followers {
+			currentFollowerIDs = append(currentFollowerIDs, follower.GetLogin())
+		}
+		currentPage++
+	}
+
+	log.Info("Total followers: %d", len(currentFollowerIDs))
+
+	// Load the gist follower database.
+	gistID := os.Getenv("GIST_ID")
+	gist, _, err := client.Gists.Get(ctx, gistID)
+	if err != nil {
+		log.Fatal("Failed to get gist: %v", err)
+	}
+	// Get the first file as the gist follower database.
+	var gistFollowerFile *github.GistFile
+	for _, file := range gist.Files {
+		gistFollowerFile = &file
+		break
+	}
+	fileName := gistFollowerFile.GetFilename()
+	content := gistFollowerFile.GetContent()
+
+	previousFollowerIDs := strings.Split(content, "\n")
+	// Diff the two lists.
+	unfollowed, newFollowed := lo.Difference(previousFollowerIDs, currentFollowerIDs)
+	if len(newFollowed) > 0 {
+		log.Trace("Got %d new followers! 🎉", len(newFollowed))
+	}
+	if len(unfollowed) > 0 {
+		log.Warn("Got %d unfollowers! 😢", len(unfollowed))
+		// Save it!
+		unfollowedEventFile := gist.Files[unfollowedEventFileName]
+		unfollowedListContent := unfollowedEventFile.GetContent()
+		unfollowedListContent = strings.Join(unfollowed, "\n") + "\n" + unfollowedListContent
+		unfollowedEventFile.Content = &unfollowedListContent
+		gist.Files[unfollowedEventFileName] = unfollowedEventFile
+	}
+
+	// Save the followers to gist database.
+	newContent := strings.Join(currentFollowerIDs, "\n")
+	gistFollowerFile.Content = &newContent
+	gist.Files[github.GistFilename(fileName)] = *gistFollowerFile
+	_, _, err = client.Gists.Edit(ctx, gistID, gist)
+	if err != nil {
+		log.Fatal("Failed to update gist: %v", err)
+	}
 }
